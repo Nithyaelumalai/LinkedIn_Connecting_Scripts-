ef03a200414a0d6b0dfd4848dda7aafc962e0419
Nithyaelumalai <127472496+Nithyaelumalai@users.noreply.github.com>
2024-05-20 17:13:23 +0530

    Add files via upload

---
+++ LinkedInconnection_Script/bot-linkedin-connection/bot-linkedin-main/src/main/java/org/openqa/selenium/PrettyTable.java
@@ -0,0 +1,14 @@
+package org.openqa.selenium;
+
+public class PrettyTable {
+
+	public PrettyTable(String string, String string2, String string3, String string4, String string5) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void addRow(String string, String nameText, String trimString, String locationText, String buttonStatus) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
